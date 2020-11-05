042c858fccebc6f963825e26be6570b351ce6f90
oskarjoss <oskar.sjolin.joss@gmail.com>
2020-11-05 12:05:02 +0100

    score and win condition

--- controllers/pong.js
+++ controllers/pong.js
@@ -4,6 +4,7 @@ const pongController = function (data) {
   const socket = this;
   const io = require("../socket").getIO();
 
+  //set ball starting direction in gameState here
   if (data.action === "start game") {
     const alreadySent = gameStates[socket.room];
     //get socket ids from users connected to this room
@@ -11,10 +12,21 @@ const pongController = function (data) {
     const gameState = {
       playerOne: players[0],
       playerTwo: players[1],
+      score: {
+        playerOne: 0,
+        playerTwo: 0,
+      },
       winner: false,
     };
     gameStates[socket.room] = gameState;
 
+    if (socket.id === gameState.playerOne) {
+      socket.opponent = gameState.playerTwo;
+    } else {
+      socket.opponent = gameState.playerOne;
+    }
+
+    //send initial gameState to room once
     if (!alreadySent) {
       io.to(socket.room).emit("pong-game", {
         action: "initial gameState",
@@ -24,7 +36,6 @@ const pongController = function (data) {
   }
 
   if (data.action === "ball position") {
-    console.log(data);
     io.to(socket.room).emit("pong-game", {
       action: "new ball position",
       posX: data.posX,
@@ -35,21 +46,35 @@ const pongController = function (data) {
   }
 
   if (data.action === "pad position") {
-    let otherPlayer;
-    const gameState = gameStates[socket.room];
+    io.to(socket.opponent).emit("pong-game", {
+      action: "opponent pad position",
+      position: data.position,
+    });
+  }
 
-    //got a problem with gameState being undefined some times
-    if (gameState) {
-      if (socket.id === gameState.playerOne) {
-        otherPlayer = gameState.playerTwo;
-      } else {
-        otherPlayer = gameState.playerOne;
-      }
-      io.to(otherPlayer).emit("pong-game", {
-        action: "opponent pad position",
-        position: data.position,
-      });
+  //check for win here
+  if (data.action === "lost point") {
+    const gameState = gameStates[socket.room];
+    let winner;
+
+    if (socket.id === gameState.playerOne) {
+      gameState.score.playerTwo += 1;
+    } else {
+      gameState.score.playerOne += 1;
     }
+
+    if (gameState.score.playerOne >= 3) {
+      gameState.winner = gameState.playerOne;
+    }
+    if (gameState.score.playerTwo >= 3) {
+      gameState.winner = gameState.playerTwo;
+    }
+    gameStates[socket.room] = gameState;
+
+    io.to(socket.room).emit("pong-game", {
+      action: "point scored",
+      gameState: gameState,
+    });
   }
 };
 
